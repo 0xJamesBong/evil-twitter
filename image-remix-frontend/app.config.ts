0b7a076c3490eead610c6ed766bbaeede4770f1a
oxjamesbong <oxjamesbong@protonmail.com>
2025-06-14 17:53:05 -0500

    app config modification

--- image-remix-frontend/app.config.ts
+++ image-remix-frontend/app.config.ts
@@ -1,20 +1,20 @@
-import 'dotenv/config'
+import "dotenv/config";
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase configuration in .env.local')
+  throw new Error("Missing Supabase configuration in .env.local");
 }
 
 export default {
   expo: {
-    name: 'image-remix-frontend',
-    slug: 'image-remix-frontend',
-    version: '1.0.0',
+    name: "image-remix-frontend",
+    slug: "image-remix-frontend",
+    version: "1.0.0",
     extra: {
       supabaseUrl,
       supabaseAnonKey,
     },
   },
-}
+};
