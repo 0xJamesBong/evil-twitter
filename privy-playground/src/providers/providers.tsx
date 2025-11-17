adf14ba85e76eacffd707011fa94effbaf44d4b2
oxjamesbong <oxjamesbong@protonmail.com>
2025-11-16 22:17:53 -0500

    privy

---
+++ privy-playground/src/providers/providers.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    // <PrivyProvider
+    //   appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+    //   clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
+    //   config={{
+    //     embeddedWallets: {
+    //       ethereum: {
+    //         createOnLogin: "users-without-wallets",
+    //       },
+    //       solana: {
+    //         createOnLogin: "users-without-wallets",
+    //       },
+    //     },
+    //     appearance: { walletChainType: "ethereum-and-solana" },
+    //     externalWallets: { solana: { connectors: toSolanaWalletConnectors() } },
+    //   }}
+    // >
+    //   {children}
+    // </PrivyProvider>
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
+      config={{
+        "appearance": {
+          "accentColor": "#6A6FF5",
+          "theme": "#FFFFFF",
+          "showWalletLoginFirst": false,
+          "logo": "https://auth.privy.io/logos/privy-logo.png",
+          "walletChainType": "ethereum-and-solana",
+          "walletList": [
+            // "detected_ethereum_wallets",
+            "detected_solana_wallets",
+            "metamask",
+            "phantom",
+            // "coinbase_wallet",
+            // "base_account",
+            // "rainbow",
+            // "solflare",
+            // "backpack",
+            // "okx_wallet",
+            // "wallet_connect"
+          ]
+        },
+        "loginMethods": [
+          "email",
+          "wallet"
+        ],
+        "fundingMethodConfig": {
+          "moonpay": {
+            "useSandbox": true
+          }
+        },
+        "embeddedWallets": {
+          "showWalletUIs": true,
+          "ethereum": {
+            "createOnLogin": "off"
+          },
+          "solana": {
+            "createOnLogin": "users-without-wallets"
+          }
+        },
+        "mfa": {
+          "noPromptOnMfaRequired": false
+        },
+        externalWallets: { solana: { connectors: toSolanaWalletConnectors() } },
+
+        // "solana": {
+        //   "rpcs": {
+        //     "solana:mainnet": {
+        //       "rpc": {},
+        //       "rpcSubscriptions": {}
+        //     }
+        //   }
+        // }
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
+
+
+
